a8caea5c0b4dcd62cabc124cb882d2a276673938
szchen23 <156382133+szchen23@users.noreply.github.com>
2024-02-13 13:48:58 -0800

    Update oblivious.sql

--- oblivious.sql
+++ oblivious.sql
@@ -1,18 +1,26 @@
 --create Node table
 CREATE TABLE Node(
-	ID INT NOT NULL PRIMARY KEY,
-	FirstName VARCHAR(255) NOT NULL,
-	LastName VARCHAR(255) NOT NULL,
-	Age INT CHECK (Age BETWEEN 0 AND 100),
-	DOB DATE,
-	FatherName VARCHAR(255),
-	MotherName VARCHAR(255),
+	id SERIAL NOT NULL PRIMARY KEY,
+	parent_id INT,
+	label VARCHAR(20),
+	value VARCHAR(255),
+	FOREIGN KEY (parent_id) REFERENCES Node (id),
+	CONSTRAINT CC1 CHECK(TYPE IN ("element", "attribute"))
 )
 
 --insert data from tree1.xml
-INSERT INTO Node (ID, FirstName, LastName, Age, DOB, FatherName, Mothername)
-VALUES (373054, 'Jason', 'Lee', 29, '1995-02-03', 'Jacky Lee', 'Lisa Lee')
-
+INSERT INTO Node (parent_id, label, value)
+VALUES 
+	(NULL, 'family', NULL),
+	(1, 'person', NULL),
+	(2, 'ID', 373054),
+	(2, 'FirstName', 'Jason'), 
+	(2, 'LastName', 'Lee'), 
+	(2, 'Age', 29)
+	(2, 'DOB', '1995-02-03'), 
+	(2, 'FatherName', 'Jacky Lee'), 
+	(2, 'MotherName', 'Lisa Lee')
+	 
 --query for persons born between 1970 and 1980
 SELECT * 
 FROM Node
